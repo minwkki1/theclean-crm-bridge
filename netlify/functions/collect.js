2dd86c2e566e4920273c6a3e46e1b7d69f35f3ae
minwkki1 <minwkki@gmail.com>
2026-04-10 15:07:22 +0900

    Update collect.js

--- netlify/functions/collect.js
+++ netlify/functions/collect.js
@@ -17,6 +17,7 @@ const ALLOW_ORIGINS = [
   "https://ming709826297.imweb.me",
   "https://xn--9m1bq4jd2k55kh7g.kr",
   "https://www.xn--9m1bq4jd2k55kh7g.kr",
+  "https://thecleanathome.co.kr",  
 ];
 
 function pickOrigin(headers) {
